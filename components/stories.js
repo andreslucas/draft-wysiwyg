2f270ff25e5a98ac15d6152a805e55828fc6d874
Andrés Lucas <alucas@iMac-de-Andres.local>
2016-11-17 16:10:18 +0100

    ADD storybook to test the component

---
+++ components/stories.js
@@ -0,0 +1,21 @@
+/**
+ * @license Copyright (c) UserZoom 2016. All Rights Reserved. http://www.userzoom.com/
+ *
+ * Proprietary and confidential
+ *
+ * NOTICE: All information contained herein is, and remains the property
+ * of UserZoom Technologies SL. The intellectual and technical concepts
+ * contained herein are proprietary to UserZoom Technologies SL and
+ * may be covered by U.S. and Foreign Patents, patents in process, and are
+ * protected by trade secret or copyright law. Dissemination of this
+ * information or reproduction of this material is strictly forbidden unless
+ * prior written permission is obtained from UserZoom Technologies SL.
+ *
+ * @company UserZoom Technologies SL
+ * @project draft-wysiwyg
+ * @file    stories.js
+ * @author  UserZoom R&D
+ * @email   rd@userzoom.com
+ * @date    2016-11-17
+ * @summary empty.
+ */
